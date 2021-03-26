d49f01b7548c39a8e92afc61012ac9b5b49b1047
dshaw1242 <david.shaw1242@gmail.com>
2021-03-25 20:39:11 -0600

    setup password length function

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,23 @@
 // prompted with password criteria
 
 // set length of password (between 8 and 128 characters)
+function passwordLength(){
+  while(true){
+    var passLength = parseInt(prompt("how long do you want your password? Between 8 and 128 characters"));
 
+   if(passLength === null){
+    //  user hit cancel
+    alert("Please enter a number");
+    } else {
+      if(passLength === "" || isNaN(passLength) || passLength < 8 || passLength > 128){
+        alert("Please enter a valid number.");
+      } else {
+        // valid entry
+        return passLength;
+      }
+    }
+  }
+}
 // prompt for special character types
 
 // user selects lowercase, uppercase, numeric, and/or special characters
@@ -11,7 +27,11 @@
 // validate input from user and at least one of each selected type should be included
 
 // then generate password and display in either the box or as an alert
-
+function generatePassword(){
+  var passLength = passwordLength();
+  console.log(passLength);
+  return passwordLength;
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
